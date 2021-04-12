2496b0575322173ba9abba1be0a1b93fe0726896
Ivan <ivan.kashytski.mkx@gmail.com>
2021-04-12 17:23:34 +0300

    Активация по расстоянию луча

--- Assets/Scripts/RayCast.cs
+++ Assets/Scripts/RayCast.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class RayCast : MonoBehaviour
 {
     public float rayDistance;
-    
+
+
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         Debug.DrawRay(transform.position, ray.direction * rayDistance);
-
+     
         if (Physics.Raycast(ray, rayDistance))
         {
             RaycastHit hit;
             Physics.Raycast(ray, out hit);
+
+            if (hit.collider.gameObject.tag == "object")
+            {
+                Button but1;
+                but1 = hit.collider.gameObject.GetComponent<Button>();
+                but1.enabled = true;
+            }
         }
     }
 }
