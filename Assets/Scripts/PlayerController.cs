c209a03d43534de4cf5f4000842460a1b2e3572d
Uko1ove <nymrod74@gmail.com>
2021-04-17 22:24:48 +0300

    added furniture, skybox, iPad

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Camera mainCamera;
     private float playerSpeed = 10f;
-    private float mouseSpeed = 100f;
+    private float mouseSpeed = 120f;
     private float horizontal;
     private float vertical;
     private float mouseX;
-    private float mouseY;
-
+    public float mouseY;
+    private float cameraRotation;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +24,19 @@ public class PlayerController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
         mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        cameraRotation = mainCamera.transform.rotation.x;
+        
         transform.Translate(Vector3.right * Time.deltaTime * playerSpeed * horizontal);
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed * vertical);
         transform.Rotate(Vector3.up * mouseSpeed * Time.deltaTime * mouseX);
+
+        // Какая-то жопа. Не понимаю, как сделать ограничения на вертикальное вращение камеры.
+        mainCamera.transform.Rotate(-Vector3.right * mouseSpeed * Time.deltaTime * mouseY);
+
+
+
+
+
     }
 }
