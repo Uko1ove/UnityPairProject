2069f1550a640524471fbdf53eef2e2e05c20afc
Ivan <ivan.kashytski.mkx@gmail.com>
2021-04-19 19:55:29 +0300

    Кот, звуки, рандомное перемещение, избегание препятствий

--- Assets/Scripts/CatScript.cs
+++ Assets/Scripts/CatScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CatScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public AudioSource audio1;
+    public int cat_speed = 10;
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * cat_speed);
+        Ray ray1 = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray1, 4))
+        {
+            audio1.Play();
+
+            if (Random.RandomRange(0, 2) == 0)
+                transform.rotation = Quaternion.Euler(0, transform.localRotation.eulerAngles.y + 90, 0);
+            else
+                transform.rotation = Quaternion.Euler(0, transform.localRotation.eulerAngles.y - 90, 0);
+        }
     }
 }
