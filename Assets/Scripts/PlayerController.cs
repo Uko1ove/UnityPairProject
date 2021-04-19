780324f28205f8db6a5e2abc3715f2314661760d
Uko1ove <nymrod74@gmail.com>
2021-04-19 16:24:47 +0300

    added ciPadCanvasScreen + vertical rotation fix + txt file

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
-    private float playerSpeed = 10f;
-    private float mouseSpeed = 120f;
+    private const float playerSpeed = 10f;
+    private const float mouseSpeed = 120f;
     private float horizontal;
     private float vertical;
     private float mouseX;
     public float mouseY;
     private float cameraRotation;
+    private float rotationX = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
@@ -30,13 +31,10 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.right * Time.deltaTime * playerSpeed * horizontal);
         transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed * vertical);
         transform.Rotate(Vector3.up * mouseSpeed * Time.deltaTime * mouseX);
-
-        // Какая-то жопа. Не понимаю, как сделать ограничения на вертикальное вращение камеры.
-        mainCamera.transform.Rotate(-Vector3.right * mouseSpeed * Time.deltaTime * mouseY);
-
-
-
-
+        
+        rotationX += mouseY * mouseSpeed * Time.deltaTime;
+        rotationX = Mathf.Clamp(rotationX, -30, 30);
+        mainCamera.transform.localRotation = Quaternion.Euler(-rotationX, 0, 0);
 
     }
 }
