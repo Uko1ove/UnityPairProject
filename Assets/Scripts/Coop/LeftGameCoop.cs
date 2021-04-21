aecedd2b32329a3bc4db4bcb8f0c5776f5d0fad3
Ivan <ivan.kashytski.mkx@gmail.com>
2021-04-21 18:33:42 +0300

    _

--- Assets/Scripts/Coop/LeftGameCoop.cs
+++ Assets/Scripts/Coop/LeftGameCoop.cs
@@ -7,16 +7,18 @@ using Photon.Realtime;
 
 public class LeftGameCoop : MonoBehaviourPunCallbacks
 {
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.Instantiate(player.name, new Vector3(15,0,1), Quaternion.identity );
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void Leave()
