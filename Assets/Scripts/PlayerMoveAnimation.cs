ebe30ed344e7113fcbb1fe0abff301211cc6d02f
Ivan <ivan.kashytski.mkx@gmail.com>
2021-04-26 21:58:58 +0300

    БагиСейфа

--- Assets/Scripts/PlayerMoveAnimation.cs
+++ Assets/Scripts/PlayerMoveAnimation.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerMoveAnimation : MonoBehaviour
 {
     private Animator anim1;
+    private PhotonView photonView;
 
     private void Start()
     {
         anim1 = GetComponent<Animator>();
+        photonView = GetComponent<PhotonView>();
     }
 
     private void Update()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) )
         {
-            anim1.SetBool("isMove",true);
+            if (photonView.IsMine)
+                anim1.SetBool("isMove",true);
         }
         else
         {
-            anim1.SetBool("isMove", false);
+            if (photonView.IsMine)
+                anim1.SetBool("isMove", false);
         }
     }
 }
--- Assets/Scripts/Safe/OpenSafe.cs
+++ Assets/Scripts/Safe/OpenSafe.cs
@@ -9,27 +9,21 @@ public class OpenSafe : MonoBehaviour, IInteractable
     public Text num2;
     public Text num3;
     public Text num4;
-    public AudioSource audio1;
-    public GameObject HandleRotate;
 
+    public AudioSource audio1;
     Animator anim1;
-    Animator anim2;
 
     public void Interact()
     {
         anim1 = GetComponent<Animator>();
-        anim2 = HandleRotate.GetComponent<Animator>();
 
-        if (anim1.enabled == false && anim2.enabled == false) Open();
+        if (anim1.enabled == false) Open();
     }
     public void Open()
     {
         if (num1.text == "1" && num2.text == "4" && num3.text == "8" && num4.text == "2")
         {
             anim1.enabled = true;
-
-            anim2.enabled = true;
-
             audio1.Play();
         }
     }
