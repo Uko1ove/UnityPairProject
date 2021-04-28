4192afb5fef762efabad21fd15751aa237887ab5
Uko1ove <nymrod74@gmail.com>
2021-04-28 16:10:10 +0300

    Fixed main menu scripts and click sound

--- Assets/Scripts/MenuScene/SettingsButtons/SoundButton.cs
+++ Assets/Scripts/MenuScene/SettingsButtons/SoundButton.cs
@@ -5,26 +5,12 @@ using UnityEngine.UI;
 
 public class SoundButton : MonoBehaviour
 {
-    public GameObject SettingMenu;
-    public GameObject SoundMenu;
+    [SerializeField] GameObject settingsMenu;
+    [SerializeField] GameObject soundMenu;
 
     public void SoundSetting()
     {
-        Text text1 = GetComponentInChildren<Text>();
-
-        if (SettingMenu.activeInHierarchy == true)
-        {
-            text1.text = "Back";
-
-            SettingMenu.SetActive(false);
-            SoundMenu.SetActive(true);
-        }
-        else
-        {
-            text1.text = "Sound";
-
-            SettingMenu.SetActive(true);
-            SoundMenu.SetActive(false);
-        }
+        settingsMenu.SetActive(!settingsMenu.activeSelf);
+        soundMenu.SetActive(!soundMenu.activeSelf);
     }
 }
