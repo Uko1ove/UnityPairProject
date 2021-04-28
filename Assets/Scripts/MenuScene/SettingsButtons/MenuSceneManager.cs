428f290006d58d0a46e52f587e48fa015add2ff4
Uko1ove <nymrod74@gmail.com>
2021-04-29 00:59:56 +0300

    Localization of main menu

--- Assets/Scripts/MenuScene/SettingsButtons/MenuSceneManager.cs
+++ Assets/Scripts/MenuScene/SettingsButtons/MenuSceneManager.cs
@@ -7,6 +7,24 @@ public class MenuSceneManager : MonoBehaviour
 {
     public GameObject ContinueButton;
     [SerializeField] Text continueText;
+    [SerializeField] Text newGameText;
+    [SerializeField] Text coopText;
+    [SerializeField] Text serverText;
+    [SerializeField] Text clientText;
+    [SerializeField] Text settingsText;
+    [SerializeField] Text exitText;
+    [SerializeField] Text languageText;
+    [SerializeField] Text engText;
+    [SerializeField] Text rusText;
+    [SerializeField] Text backLanguageText;
+    [SerializeField] Text soundText;
+    [SerializeField] Text bgSoundText;
+    [SerializeField] Text clickSoundText;
+    [SerializeField] Text backSoundText;
+    [SerializeField] Text resetText;
+    [SerializeField] Text yesText;
+    [SerializeField] Text noText;
+    [SerializeField] Text backSettingsText;
 
     string language;
 
@@ -22,10 +40,52 @@ public class MenuSceneManager : MonoBehaviour
         if (language == "eng")
         {
             continueText.text = "Continue";
+            newGameText.text = "New Game";
+            coopText.text = "Coop";
+            serverText.text = "Server";
+            clientText.text = "Client";
+            settingsText.text = "Settings";
+            exitText.text = "Exit";
+
+            languageText.text = "Language";
+            engText.text = "English";
+            rusText.text = "Russian";
+            backLanguageText.text = "Back";
+
+            soundText.text = "Sound";
+            bgSoundText.text = "BackSound";
+            clickSoundText.text = "ClickSound";
+            backSoundText.text = "Back";
+
+            resetText.text = "Reset Progress";
+            yesText.text = "Yes";
+            noText.text = "No";
+            backSettingsText.text = "Back";
         }
         else
         {
             continueText.text = "Продолжить";
+            newGameText.text = "Новая игра";
+            coopText.text = "Сеть";
+            serverText.text = "Сервер";
+            clientText.text = "Клиент";
+            settingsText.text = "Настройки";
+            exitText.text = "Выход";
+
+            languageText.text = "Язык";
+            engText.text = "Английский";
+            rusText.text = "Русский";
+            backLanguageText.text = "Назад";
+
+            soundText.text = "Звук";
+            bgSoundText.text = "Фон";
+            clickSoundText.text = "Клик";
+            backSoundText.text = "Назад";
+
+            resetText.text = "Сброс прогресса";
+            yesText.text = "Да";
+            noText.text = "Нет";
+            backSettingsText.text = "Назад";
         }
     }
 }
