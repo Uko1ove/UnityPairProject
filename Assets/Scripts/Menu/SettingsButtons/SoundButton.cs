98b89d0d063d349fd5a2d6d6b1f4084cfbe28691
Ivan <ivan.kashytski.mkx@gmail.com>
2021-04-28 12:01:48 +0300

    немного поправил якоря

--- Assets/Scripts/Menu/SettingsButtons/SoundButton.cs
+++ Assets/Scripts/Menu/SettingsButtons/SoundButton.cs
@@ -5,38 +5,26 @@ using UnityEngine.UI;
 
 public class SoundButton : MonoBehaviour
 {
-    public GameObject goOut_0;
-    public GameObject goOut_1;
-    public GameObject goOut_2;
-
-    public GameObject goIn_0;
-    public GameObject goIn_1;
+    public GameObject SettingMenu;
+    public GameObject SoundMenu;
 
     public void SoundSetting()
     {
         Text text1 = GetComponentInChildren<Text>();
 
-        if (goOut_0.activeInHierarchy == true)
+        if (SettingMenu.activeInHierarchy == true)
         {
             text1.text = "Back";
 
-            goOut_0.SetActive(false);
-            goOut_1.SetActive(false);
-            goOut_2.SetActive(false);
-
-            goIn_0.SetActive(true);
-            goIn_1.SetActive(true);
+            SettingMenu.SetActive(false);
+            SoundMenu.SetActive(true);
         }
         else
         {
             text1.text = "Sound";
 
-            goOut_0.SetActive(true);
-            goOut_1.SetActive(true);
-            goOut_2.SetActive(true);
-
-            goIn_0.SetActive(false);
-            goIn_1.SetActive(false);
+            SettingMenu.SetActive(true);
+            SoundMenu.SetActive(false);
         }
     }
 }
