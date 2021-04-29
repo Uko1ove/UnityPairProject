2898fa5f871c3cddc07735a1d9ad1b0638bc0291
Ivan <ivan.kashytski.mkx@gmail.com>
2021-04-29 13:10:04 +0300

    Локализация готова

--- Assets/Scripts/MenuScene/MenuSceneManager.cs
+++ Assets/Scripts/MenuScene/MenuSceneManager.cs
@@ -3,31 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuSceneManager : MonoBehaviour
+public class MenuSceneManager : ChangeLocalization
 {
     [SerializeField] AudioSource audioBG;
-    public GameObject ContinueButton;
+    public GameObject ContinueVisible;
     [SerializeField] GameObject Slider;
-
-    [SerializeField] Text continueText;
-    [SerializeField] Text newGameText;
-    [SerializeField] Text coopText;
-    [SerializeField] Text serverText;
-    [SerializeField] Text clientText;
-    [SerializeField] Text settingsText;
-    [SerializeField] Text exitText;
-    [SerializeField] Text languageText;
-    [SerializeField] Text engText;
-    [SerializeField] Text rusText;
-    [SerializeField] Text backLanguageText;
-    [SerializeField] Text soundText;
-    [SerializeField] Text bgSoundText;
-    [SerializeField] Text clickSoundText;
-    [SerializeField] Text backSoundText;
-    [SerializeField] Text resetText;
-    [SerializeField] Text yesText;
-    [SerializeField] Text noText;
-    [SerializeField] Text backSettingsText;
 
     string language;
 
@@ -46,60 +26,18 @@ public class MenuSceneManager : MonoBehaviour
         string player = PlayerPrefs.GetString("player");
         string invent = PlayerPrefs.GetString("invent");
 
-        if (player == "5 -26 0" && invent == "00000") try { ContinueButton.SetActive(false); } catch { };
+        if (player == "5 -26 0" && invent == "00000") try { ContinueVisible.SetActive(false); } catch { };
 
         //Загрузка локализации
         language = PlayerPrefs.GetString("local", "eng");
 
         if (language == "eng")
         {
-            continueText.text = "Continue";
-            newGameText.text = "New Game";
-            coopText.text = "Coop";
-            serverText.text = "Server";
-            clientText.text = "Client";
-            settingsText.text = "Settings";
-            exitText.text = "Exit";
-
-            languageText.text = "Language";
-            engText.text = "English";
-            rusText.text = "Russian";
-            backLanguageText.text = "Back";
-
-            soundText.text = "Sound";
-            bgSoundText.text = "BackSound";
-            clickSoundText.text = "ClickSound";
-            backSoundText.text = "Back";
-
-            resetText.text = "Reset Progress";
-            yesText.text = "Yes";
-            noText.text = "No";
-            backSettingsText.text = "Back";
+            LocalizeToEng();
         }
         else
         {
-            continueText.text = "Продолжить";
-            newGameText.text = "Новая игра";
-            coopText.text = "Сеть";
-            serverText.text = "Сервер";
-            clientText.text = "Клиент";
-            settingsText.text = "Настройки";
-            exitText.text = "Выход";
-
-            languageText.text = "Язык";
-            engText.text = "Английский";
-            rusText.text = "Русский";
-            backLanguageText.text = "Назад";
-
-            soundText.text = "Звук";
-            bgSoundText.text = "Фон";
-            clickSoundText.text = "Клик";
-            backSoundText.text = "Назад";
-
-            resetText.text = "Сброс прогресса";
-            yesText.text = "Да";
-            noText.text = "Нет";
-            backSettingsText.text = "Назад";
+            LocalizeToRus();
         }
     }
 }
