0be669a97db313b56af04fea79f5453302843da0
Ivan <ivan.kashytski.mkx@gmail.com>
2021-04-29 22:55:00 +0300

    Продолжаю придумывать себе задачи

--- Assets/Scripts/MenuScene/MenuSceneManager.cs
+++ Assets/Scripts/MenuScene/MenuSceneManager.cs
@@ -39,5 +39,8 @@ public class MenuSceneManager : ChangeLocalization
         {
             LocalizeToRus();
         }
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
